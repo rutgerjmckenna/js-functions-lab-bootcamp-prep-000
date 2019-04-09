06733160f91be60f6584183a85aebb27760e0139
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-09 21:09:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ return "Happy holidays!"
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
-function happyHolidaysTo(holiday, name) {
-  console.log('Happy ${holiday}, ${name}!')
-}
 function holidayCountdown(days, holiday) {
   console.log("It's ${days} days until ${holiday}!")
 }
