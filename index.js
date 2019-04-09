bf3cbb3e96067a1545192053ca83ecb51334aaae
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-09 20:36:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// write your code below!
+function happyHolidays(string) {
+return "Happy holidays!"
+}
+function happyHolidaysTo(name) {
+  console.log("Happy holidays, ${name}!")
+}
+happyHolidaysTo('you')
+function happyHolidaysTo(holiday, name) {
+  console.log('Happy ${holiday}, ${name}!')
+}
